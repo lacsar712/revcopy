bd88a574b4be87d83e6e1bb55c8f6e680c868ce4
selfbuilt <selfbuilt@local>
2026-08-16 22:25:40 +0800

    bug

--- internal/rev/rev.go
+++ internal/rev/rev.go
@@ -2,8 +2,9 @@ package rev
 
 // Reverse returns a reversed copy of s (must not mutate s).
 func Reverse(s []int) []int {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+	out := make([]int, len(s))
+	for i := range s {
+		out[len(s)-1-i] = s[i]
 	}
-	return s
+	return out
 }
